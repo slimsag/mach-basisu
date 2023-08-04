1fdbcbf7d2dc9ad21a38b1093d0a4b452f5933ab
Stephen Gutekanst <stephen@hexops.com>
2023-08-04 08:07:43 -0700

    workaround transitive dependency issue
    See hexops/mach#902
    
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- build.zig
+++ build.zig
@@ -9,35 +9,38 @@ pub fn build(b: *Build) void {
         .source_file = .{ .path = "src/main.zig" },
     });
 
-    // TODO: add this as a module dependency
-    const lib = b.addStaticLibrary(.{
-        .name = "mach-basisu",
-        .target = target,
-        .optimize = optimize,
-    });
+    // TODO: we cannot call b.dependency("basisu") inside of `pub fn build`
+    // if we want users of the package to be able to make use of it.
+    // See hexops/mach#902
+    _ = optimize;
+    _ = target;
+    // // TODO: add this as a module dependency
+    // const lib = b.addStaticLibrary(.{
+    //     .name = "mach-basisu",
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
+    // lib.linkLibCpp();
+    // lib.linkLibrary(b.dependency("basisu", .{
+    //     .target = target,
+    //     .optimize = optimize,
+    // }).artifact("basisu"));
 
-    lib.linkLibCpp();
+    // lib.addCSourceFiles(&.{
+    //     "src/encoder/wrapper.cpp",
+    //     "src/transcoder/wrapper.cpp",
+    // }, &.{});
 
-    lib.linkLibrary(b.dependency("basisu", .{
-        .target = target,
-        .optimize = optimize,
-    }).artifact("basisu"));
+    // b.installArtifact(lib);
 
-    lib.addCSourceFiles(&.{
-        "src/encoder/wrapper.cpp",
-        "src/transcoder/wrapper.cpp",
-    }, &.{});
+    // const test_exe = b.addTest(.{
+    //     .root_source_file = .{ .path = "src/main.zig" },
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
 
-    b.installArtifact(lib);
+    // test_exe.linkLibrary(lib);
 
-    const test_exe = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-
-    test_exe.linkLibrary(lib);
-
-    const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&b.addRunArtifact(test_exe).step);
+    // const test_step = b.step("test", "Run library tests");
+    // test_step.dependOn(&b.addRunArtifact(test_exe).step);
 }
