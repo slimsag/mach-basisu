1dac12fffee96af962aece3c3d746c84e1b2c662
Stephen Gutekanst <stephen@hexops.com>
2023-08-04 08:46:32 -0700

    workaround transitive dependency issue (hexops/mach#902)
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- build.zig
+++ build.zig
@@ -44,3 +44,35 @@ pub fn build(b: *Build) void {
     // const test_step = b.step("test", "Run library tests");
     // test_step.dependOn(&b.addRunArtifact(test_exe).step);
 }
+
+// TODO: remove this once hexops/mach#902 is fixed.
+pub fn lib(
+    b: *Build,
+    optimize: std.builtin.OptimizeMode,
+    target: std.zig.CrossTarget,
+) *std.Build.Step.Compile {
+    const l = b.addStaticLibrary(.{
+        .name = "mach-basisu",
+        .target = target,
+        .optimize = optimize,
+    });
+    l.linkLibCpp();
+    l.linkLibrary(b.dependency("basisu", .{
+        .target = target,
+        .optimize = optimize,
+    }).artifact("basisu"));
+
+    l.addCSourceFiles(&.{
+        sdkPath("/src/encoder/wrapper.cpp"),
+        sdkPath("/src/transcoder/wrapper.cpp"),
+    }, &.{});
+    return l;
+}
+
+fn sdkPath(comptime suffix: []const u8) []const u8 {
+    if (suffix[0] != '/') @compileError("suffix must be an absolute path");
+    return comptime blk: {
+        const root_dir = std.fs.path.dirname(@src().file) orelse ".";
+        break :blk root_dir ++ suffix;
+    };
+}
