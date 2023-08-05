0339e62dc3a8428f74ac092d4930bc79a594ba7b
Ali Chraghi <alichraghi@proton.me>
2023-08-05 06:38:59 +0330

    banish usingnamespace

--- src/main.zig
+++ src/main.zig
@@ -1,27 +1,29 @@
-pub usingnamespace @import("encoder.zig");
-pub usingnamespace @import("transcoder.zig");
+pub const init_encoder = @import("encoder.zig").init_encoder;
+pub const Compressor = @import("encoder.zig").Compressor;
+pub const CompressorParams = @import("encoder.zig").CompressorParams;
+pub const Image = @import("encoder.zig").Image;
+pub const PackUASTCFlags = @import("encoder.zig").PackUASTCFlags;
+pub const ColorSpace = @import("encoder.zig").ColorSpace;
+pub const init_transcoder = @import("transcoder.zig").init_transcoder;
+pub const isFormatEnabled = @import("transcoder.zig").isFormatEnabled;
+pub const Transcoder = @import("transcoder.zig").Transcoder;
 
 pub const BasisTextureFormat = enum(u1) {
     etc1s = 0,
     uastc4x4 = 1,
 };
 
-// Test
-
-const t = @import("transcoder.zig");
-const e = @import("encoder.zig");
 const testing = @import("std").testing;
 
 test "reference decls" {
-    testing.refAllDeclsRecursive(t);
-    testing.refAllDeclsRecursive(e);
+    testing.refAllDeclsRecursive(@This());
 }
 
 test "encode/transcode" {
     // Encode
-    e.init_encoder();
+    init_encoder();
 
-    const params = e.CompressorParams.init(1);
+    const params = CompressorParams.init(1);
     params.setGenerateMipMaps(true);
     params.setBasisFormat(.uastc4x4);
     params.setPackUASTCFlags(.{ .fastest = true });
@@ -30,13 +32,13 @@ test "encode/transcode" {
     const image = params.getImageSource(0);
     image.fill(@embedFile("ziggy.png"), 379, 316, 4);
 
-    const comp = try e.Compressor.init(params);
+    const comp = try Compressor.init(params);
     try comp.process();
 
     // Transcode
-    t.init_transcoder();
+    init_transcoder();
 
-    const trans = try t.Transcoder.init(comp.output());
+    const trans = try Transcoder.init(comp.output());
     defer trans.deinit();
 
     var out_buf = try testing.allocator.alloc(u8, try trans.calcTranscodedSize(0, 0, .astc_4x4_rgba));
