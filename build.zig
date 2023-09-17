fe2f30a9a9662db2d12bc978b50158d4e74ec0d8
Stephen Gutekanst <stephen@hexops.com>
2023-09-17 13:54:54 -0700

    update to latest Zig
    Helps hexops/mach#902
    
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- build.zig
+++ build.zig
@@ -9,70 +9,37 @@ pub fn build(b: *Build) void {
         .source_file = .{ .path = "src/main.zig" },
     });
 
-    // TODO: we cannot call b.dependency("basisu") inside of `pub fn build`
-    // if we want users of the package to be able to make use of it.
-    // See hexops/mach#902
-    _ = optimize;
-    _ = target;
-    // // TODO: add this as a module dependency
-    // const lib = b.addStaticLibrary(.{
-    //     .name = "mach-basisu",
-    //     .target = target,
-    //     .optimize = optimize,
-    // });
-    // lib.linkLibCpp();
-    // lib.linkLibrary(b.dependency("basisu", .{
-    //     .target = target,
-    //     .optimize = optimize,
-    // }).artifact("basisu"));
-
-    // lib.addCSourceFiles(&.{
-    //     "src/encoder/wrapper.cpp",
-    //     "src/transcoder/wrapper.cpp",
-    // }, &.{});
-
-    // b.installArtifact(lib);
-
-    // const test_exe = b.addTest(.{
-    //     .root_source_file = .{ .path = "src/main.zig" },
-    //     .target = target,
-    //     .optimize = optimize,
-    // });
-
-    // test_exe.linkLibrary(lib);
-
-    // const test_step = b.step("test", "Run library tests");
-    // test_step.dependOn(&b.addRunArtifact(test_exe).step);
-}
-
-// TODO: remove this once hexops/mach#902 is fixed.
-pub fn lib(
-    b: *Build,
-    optimize: std.builtin.OptimizeMode,
-    target: std.zig.CrossTarget,
-) *std.Build.Step.Compile {
-    const l = b.addStaticLibrary(.{
+    // TODO: add this as a module dependency
+    const lib = b.addStaticLibrary(.{
         .name = "mach-basisu",
         .target = target,
         .optimize = optimize,
     });
-    l.linkLibCpp();
-    l.linkLibrary(b.dependency("basisu", .{
+    lib.linkLibCpp();
+    link(b, lib);
+
+    lib.addCSourceFiles(&.{
+        "src/encoder/wrapper.cpp",
+        "src/transcoder/wrapper.cpp",
+    }, &.{});
+
+    b.installArtifact(lib);
+
+    const test_exe = b.addTest(.{
+        .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
         .optimize = optimize,
-    }).artifact("basisu"));
+    });
 
-    l.addCSourceFiles(&.{
-        sdkPath("/src/encoder/wrapper.cpp"),
-        sdkPath("/src/transcoder/wrapper.cpp"),
-    }, &.{});
-    return l;
+    test_exe.linkLibrary(lib);
+
+    const test_step = b.step("test", "Run library tests");
+    test_step.dependOn(&b.addRunArtifact(test_exe).step);
 }
 
-fn sdkPath(comptime suffix: []const u8) []const u8 {
-    if (suffix[0] != '/') @compileError("suffix must be an absolute path");
-    return comptime blk: {
-        const root_dir = std.fs.path.dirname(@src().file) orelse ".";
-        break :blk root_dir ++ suffix;
-    };
+pub fn link(b: *std.Build, step: *std.build.CompileStep) void {
+    step.linkLibrary(b.dependency("basisu", .{
+        .target = step.target,
+        .optimize = step.optimize,
+    }).artifact("basisu"));
 }
