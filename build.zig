27cf65582ae75613e338aac278498207f351049d
Stephen Gutekanst <stephen@hexops.com>
2024-01-13 15:18:54 -0700

    update to Zig 2024.1.0-mach
    helps hexops/mach#1135
    
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- build.zig
+++ build.zig
@@ -5,25 +5,26 @@ pub fn build(b: *Build) void {
     const optimize = b.standardOptimizeOption(.{});
     const target = b.standardTargetOptions(.{});
 
-    _ = b.addModule("mach-basisu", .{
-        .source_file = .{ .path = "src/main.zig" },
-    });
-
-    // TODO: add this as a module dependency
     const lib = b.addStaticLibrary(.{
         .name = "mach-basisu",
         .target = target,
         .optimize = optimize,
     });
     lib.linkLibCpp();
-    link(b, lib);
-
     lib.addCSourceFiles(.{ .files = &.{
         "src/encoder/wrapper.cpp",
         "src/transcoder/wrapper.cpp",
     }, .flags = &.{} });
-
+    lib.linkLibrary(b.dependency("basisu", .{
+        .target = target,
+        .optimize = optimize,
+    }).artifact("basisu"));
     b.installArtifact(lib);
+
+    const module = b.addModule("mach-basisu", .{
+        .root_source_file = .{ .path = "src/main.zig" },
+    });
+    module.linkLibrary(lib);
 
     const test_exe = b.addTest(.{
         .root_source_file = .{ .path = "src/main.zig" },
@@ -35,11 +36,4 @@ pub fn build(b: *Build) void {
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&b.addRunArtifact(test_exe).step);
-}
-
-pub fn link(b: *std.Build, step: *std.build.CompileStep) void {
-    step.linkLibrary(b.dependency("basisu", .{
-        .target = step.target,
-        .optimize = step.optimize,
-    }).artifact("basisu"));
 }
